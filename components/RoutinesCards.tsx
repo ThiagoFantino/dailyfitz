7620d258a526c60db854996bc8da03c55231db2f
Thiago Fantino <thiagofl20@hotmail.com>
2024-09-09 10:28:05 -0300

    Now photos come from links to pass them as parameters and the routines screen is available

--- components/RoutinesCards.tsx
+++ components/RoutinesCards.tsx
@@ -1,23 +1,45 @@
-import {StyleSheet, Text, View , Pressable, Image} from 'react-native'
-import React from 'react'
-import routines from '@/data/routines'
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import React from 'react';
+import routines from '@/data/routines';
+import { useNavigation } from '@react-navigation/native';
 
 const Routines = () => {
   const RoutinesData = routines;
+  const navigation = useNavigation<any>();
   return (
     <View>
-        {RoutinesData.map((item,key)=>(
-          <Pressable style={{alignItems:"center",justifyContent:"center",margin:10}} key={key}>
-            <Image style={{width:"95%",height:140,borderRadius:7}}source={item.image}
-            />
-            <Text style={{position:"absolute",color:"white",fontSize:16,fontWeight:"bold",left:20,top:20}}>{item.name}</Text>
-
-          </Pressable>
-        ))}
+      {RoutinesData.map((item, key) => (
+        <Pressable
+        onPress={() => navigation.navigate("Routine",{
+          image:item.image,
+          excersises:item.excersises,
+          id:item.id,
+        })}
+          style={{ alignItems: "center", justifyContent: "center", margin: 10 }}
+          key={key}
+        >
+          <Image
+            style={{ width: "95%", height: 140, borderRadius: 7,}}
+            source={{uri:item.image}}
+          />
+          <Text
+            style={{
+              position: "absolute",
+              color: "white",
+              fontSize: 16,
+              fontWeight: "bold",
+              left: 20,
+              top: 20,
+            }}
+          >
+            {item.name}
+          </Text>
+        </Pressable>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default Routines
+export default Routines;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
