f9822b16a6ff404b889838b081177e9a5d1c1cde
Thiago Fantino <thiagofl20@hotmail.com>
2024-10-10 11:33:26 -0300

    Now user screen stats come from the user that has id=1. It is only for test as then it will based on the id passed on the login

--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -4,7 +4,7 @@ import { MaterialIcons, FontAwesome5, Ionicons } from '@expo/vector-icons';
 
 const UserStatsScreen = () => {
   const [data, setData] = useState('');
-  const [users, setUsers] = useState([]); // Inicializar como un array vacío
+  const [user, setUser] = useState([]); // Inicializar como un array vacío
 
   useEffect(() => {
     fetchData();
@@ -12,10 +12,10 @@ const UserStatsScreen = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('http://192.168.0.117:3000/users');
+      const response = await fetch('http://192.168.0.117:3000/users/1');
       const json = await response.json();
-      setUsers(json); // Asumiendo que json es un array de usuarios
-      console.log(json[2].apellido);
+      setUser(json); // Asumiendo que json es un array de usuarios
+      console.log(json);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -29,50 +29,28 @@ const UserStatsScreen = () => {
           source={{ uri: 'https://www.w3schools.com/w3images/avatar2.png' }} 
         />
         
-        <Text style={styles.userName}>Juan Perez</Text> 
+        <Text style={styles.userName}>{`${user.nombre} ${user.apellido}`}</Text> 
 
         <View style={styles.statsContainer}>
           <View style={styles.statCard}>
             <MaterialIcons name="fitness-center" size={36} color="#4CAF50" />
-            <Text style={styles.statNumber}>20</Text>
+            <Text style={styles.statNumber}>{`${user.entrenamientos}`}</Text>
             <Text style={styles.statLabel}>Entrenamientos</Text>
           </View>
 
           <View style={styles.statCard}>
             <FontAwesome5 name="fire" size={36} color="#F44336" />
-            <Text style={styles.statNumber}>1500</Text>
+            <Text style={styles.statNumber}>{`${user.calorias}`}</Text>
             <Text style={styles.statLabel}>Calorías Quemadas</Text>
           </View>
 
           <View style={styles.statCard}>
             <Ionicons name="time" size={36} color="#2196F3" />
-            <Text style={styles.statNumber}>300</Text>
+            <Text style={styles.statNumber}>{`${user.minutos}`}</Text>
             <Text style={styles.statLabel}>Minutos</Text>
           </View>
         </View>
 
-        <View style={{ width: '100%', paddingVertical: 10 }}>
-          {users.map((user) => (
-            <View
-              key={user.id}
-              style={{
-                backgroundColor: '#fff',
-                padding: 15,
-                borderRadius: 10,
-                marginVertical: 5,
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.1,
-                shadowRadius: 5,
-                elevation: 3,
-              }}
-            >
-              <Text style={{ fontSize: 18, color: '#333' }}>
-                {user.nombre} {user.apellido}
-              </Text>
-            </View>
-          ))}
-        </View>
 
       </ScrollView>
     </SafeAreaView>
