af237c0422b6b4fd82a519dcc418ed1855d53f68
Thiago Fantino <thiagofl20@hotmail.com>
2024-12-15 12:27:34 -0300

    Added the requirements of the fields so that the user knows what is valid

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView,TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TextInput, Pressable } from 'react-native';
 import React, { useState } from 'react';
-import {backendURL} from '@/config'
-import { useNavigation } from '@react-navigation/native'
+import { backendURL } from '@/config';
+import { useNavigation } from '@react-navigation/native';
 
 interface SignUpScreenProps {
   onFormToggle?: () => void;
@@ -23,9 +23,9 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
   const [passwordView, setPasswordView] = useState(true);
   const [successMessage, setSuccessMessage] = useState('');
 
-  const passwordViewFlip=()=>{
+  const passwordViewFlip = () => {
     setPasswordView(!passwordView);
-  }
+  };
 
   function signUpRequest() {
     
@@ -86,72 +86,73 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
   }
   
 
-  function testRegexWithAdvice(regex:RegExp,error_message:string,input_value:string){
+  function testRegexWithAdvice(regex: RegExp, error_message: string, input_value: string) {
     return (!regex.test(input_value)) ? error_message : "";
   }
 
-  function testRegexWithAdviceSet(regex_set: { regex: RegExp; advice: string; }[],input_value:string){
-    if (input_value===""){
-        return "";
+  function testRegexWithAdviceSet(regex_set: { regex: RegExp; advice: string; }[], input_value: string) {
+    if (input_value === "") {
+      return "";
     }
-    for ( let i=0;i<regex_set.length;i++){
-        if (testRegexWithAdvice(regex_set[i].regex,regex_set[i].advice,input_value)!==""){
-            return regex_set[i].advice;
-        }
+    for (let i = 0; i < regex_set.length; i++) {
+      if (testRegexWithAdvice(regex_set[i].regex, regex_set[i].advice, input_value) !== "") {
+        return regex_set[i].advice;
+      }
     }
     return "";
   }
 
-  function regexWithAdvice(regex:RegExp,advice:string){
+  function regexWithAdvice(regex: RegExp, advice: string) {
     return {
-      regex:regex,
-      advice:advice}
+      regex: regex,
+      advice: advice
+    };
   }
 
-  function changeName(input:string){
-    let advice=testProfileText(input);
-    (advice!=="")?setNameError(advice):setNameError("");
+  function changeName(input: string) {
+    let advice = testProfileText(input);
+    (advice !== "") ? setNameError(advice) : setNameError("");
     setName(input);
   }
 
-  function changeSurname(input:string){
-    let advice=testProfileText(input);
-    (advice!=="")?setSurnameError(advice):setSurnameError("");
+  function changeSurname(input: string) {
+    let advice = testProfileText(input);
+    (advice !== "") ? setSurnameError(advice) : setSurnameError("");
     setSurname(input);
   }
 
-  function testProfileText(input_password:string){
-    const length_exp=regexWithAdvice(/^.{1,}$/,"El campo no puede estar vacío");
-    const words_exp=regexWithAdvice(/^[a-zA-ZÁÉÍÓÚÜÑáéíóúüñ' -]+$/,"Solo se permiten letras, espacios y caracteres como tildes, ñ, apóstrofes o guiones.");
-    const regex_set=[length_exp,words_exp];
-    return (testRegexWithAdviceSet(regex_set,input_password));
+  function testProfileText(input_password: string) {
+    const length_exp = regexWithAdvice(/^.{1,}$/, "El campo no puede estar vacío");
+    const words_exp = regexWithAdvice(/^[a-zA-ZÁÉÍÓÚÜÑáéíóúüñ' -]+$/, "Solo se permiten letras, espacios y caracteres como tildes, ñ, apóstrofes o guiones.");
+    const regex_set = [length_exp, words_exp];
+    return (testRegexWithAdviceSet(regex_set, input_password));
   }
 
-  function changeEmail(input: string){
-    let advice=testEmail(input);
-    (advice!=="")?setEmailError(advice):setEmailError("");
+  function changeEmail(input: string) {
+    let advice = testEmail(input);
+    (advice !== "") ? setEmailError(advice) : setEmailError("");
     setEmail(input);
   }
 
-  function testEmail(input_password: string){
-    const length_exp=regexWithAdvice(/^.{1,}$/,"El campo no puede estar vacío");
-    const format_exp=regexWithAdvice(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,"El email no es valido.");
-    const regex_set=[length_exp,format_exp];
-    return (testRegexWithAdviceSet(regex_set,input_password));
+  function testEmail(input_password: string) {
+    const length_exp = regexWithAdvice(/^.{1,}$/, "El campo no puede estar vacío");
+    const format_exp = regexWithAdvice(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, "El email no es valido.");
+    const regex_set = [length_exp, format_exp];
+    return (testRegexWithAdviceSet(regex_set, input_password));
   }
 
-  function changePassword(input: string){
-    let advice=testPassword(input);
-    (advice!=="")?setPasswordError(advice):setPasswordError("");
+  function changePassword(input: string) {
+    let advice = testPassword(input);
+    (advice !== "") ? setPasswordError(advice) : setPasswordError("");
     setPassword(input);
   }
 
-  function testPassword(input_password: string){
-    const length_exp=regexWithAdvice(/^.{8,}$/,"Debe ingrear como minimo 8 caracteres.");
-    const uppercase_exp=regexWithAdvice(/(?=.*[A-Z])/,"Debe incluir al menos una letra mayúscula.");
-    const lowercase_exp=regexWithAdvice(/(?=.*[a-z])/,"Debe incluir al menos una letra minúscula.");
-    const number_exp=regexWithAdvice(/(?=.*\d)/,"Debe incluir al menos un número.");
-    const special_char_exp = regexWithAdvice(/(?=.*[@$!%*?&])/,"Debe incluir al menos un carácter especial (@$!%*?&).");
+  function testPassword(input_password: string) {
+    const length_exp = regexWithAdvice(/^.{8,}$/, "Debe ingresar como mínimo 8 caracteres.");
+    const uppercase_exp = regexWithAdvice(/(?=.*[A-Z])/, "Debe incluir al menos una letra mayúscula.");
+    const lowercase_exp = regexWithAdvice(/(?=.*[a-z])/, "Debe incluir al menos una letra minúscula.");
+    const number_exp = regexWithAdvice(/(?=.*\d)/, "Debe incluir al menos un número.");
+    const special_char_exp = regexWithAdvice(/(?=.*[@$!%*?&])/, "Debe incluir al menos un carácter especial (@$!%*?&).");
     const no_whitespace_exp = regexWithAdvice(/^\S*$/, "No se permiten espacios en blanco.");
     const regex_set = [length_exp, uppercase_exp, lowercase_exp, number_exp, special_char_exp, no_whitespace_exp];
     return testRegexWithAdviceSet(regex_set, input_password);
@@ -172,6 +173,8 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
             <Text style={styles.successMessage}>{successMessage}</Text>
           </View>
         ) : null}
+        
+        {/* Nombre */}
         <View>
           <TextInput
             style={styles.input}
@@ -180,10 +183,13 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
             placeholder='Ingrese su nombre'
             placeholderTextColor={"#808080"}
           />
+          <Text style={styles.inputHint}>Debe ser un nombre válido (letras, espacios, apóstrofes o guiones).</Text>
         </View>
         <View style={styles.inputError}>
           <Text style={styles.inputErrorMenssage}>{nameError}</Text>
         </View>
+
+        {/* Apellido */}
         <View>
           <TextInput
             style={styles.input}
@@ -192,10 +198,13 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
             placeholder='Ingrese su apellido'
             placeholderTextColor={"#808080"}
           />
+          <Text style={styles.inputHint}>Debe ser un apellido válido (letras, espacios, apóstrofes o guiones).</Text>
         </View>
         <View style={styles.inputError}>
           <Text style={styles.inputErrorMenssage}>{surnameError}</Text>
         </View>
+
+        {/* Email */}
         <View>
           <TextInput
             style={styles.input}
@@ -204,10 +213,13 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
             placeholder='Ingrese un email'
             placeholderTextColor={"#808080"}
           />
+          <Text style={styles.inputHint}>Debe tener el formato: ejemplo@dominio.com</Text>
         </View>
         <View style={styles.inputError}>
           <Text style={styles.inputErrorMenssage}>{emailError}</Text>
         </View>
+
+        {/* Contraseña */}
         <View>
           <TextInput
             style={styles.inputPassword}            
@@ -217,27 +229,20 @@ const SignUpScreen: React.FC<SignUpScreenProps> = ({ onFormToggle }) => {
             placeholder='Ingrese una contraseña'
             placeholderTextColor={"#808080"}
           />
-          <Pressable style={({ pressed }) => 
-            pressed ? styles.passwordDisplayButtonPressed : styles.passwordDisplayButton} 
+          <Pressable style={({ pressed }) => pressed ? styles.passwordDisplayButtonPressed : styles.passwordDisplayButton} 
             onPress={passwordViewFlip}>
             <Image style={styles.passwordDisplayIcon} source={{ uri: 'https://storage.needpix.com/rsynced_images/eye-2387853_1280.png' }}/>
           </Pressable>
+          <Text style={styles.inputHint}>Debe incluir al menos 8 caracteres, una mayúscula, un número y un carácter especial (@$!%*?&).</Text>
         </View>
         <View style={styles.inputError}>
           <Text style={styles.inputErrorMenssage}>{passwordError}</Text>
         </View>
+
         <View>
           <Pressable
-            style={styles.changeFormButton}
-            onPress={() => navigation.navigate('Login')} 
-          >
-            <Text style={styles.changeFormButtonText}>Ingresar</Text>
-          </Pressable>
-        </View>
-        <View>
-          <Pressable style={({ pressed }) => 
-            pressed ? styles.submitButtonPressed : styles.submitButton}
-          onPress={signUpRequest}
+            style={styles.submitButton}
+            onPress={signUpRequest}
           >
             <Text style={styles.submitButtonText}>Enviar</Text>
           </Pressable>
@@ -397,5 +402,10 @@ const styles = StyleSheet.create({
       height: '100%',
       position:'absolute',
       alignSelf: 'center',
+    },
+  inputHint: {
+      fontSize: 14,
+      color: '#007bff',
+      marginVertical: 5,
     },
 });
