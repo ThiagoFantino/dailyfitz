916357a87bb3dc539e62fc8e9b43bfddb5ca4625
Thiago Fantino <thiagofl20@hotmail.com>
2025-01-16 21:16:45 -0300

    Added more options for profile Picture and changed styles in order for pictures to fit in every screen

--- screens/ProfilePictureScreen.tsx
+++ screens/ProfilePictureScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Pressable, FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, FlatList, Alert, Dimensions } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { backendURL } from '@/config';
@@ -61,7 +61,19 @@ const ProfilePictureScreen = ({ route }) => {
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/640px-Lionel_Messi_WC2022.jpg',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/400px-Cristiano_Ronaldo_2018.jpg',
     'https://images.ctfassets.net/3mv54pzvptwz/55YLwKPDnRXkqMBITRpWbC/0c2aefc04afa455c20e9ca0d209698e0/53174188191_42d4c831ae_o.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Federer_WM16_%2837%29_%2828136155830%29.jpg/245px-Federer_WM16_%2837%29_%2828136155830%29.jpg',
+    'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/272.png',
+    'https://cdn.sanity.io/images/oyf3dba6/production/9fca0bc620a2cfebe91bcb30faab277e4776f1dd-580x580.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/0/06/Brann_-_Bar%C3%A7a_Femen%C3%AD_CG3A5851_%28cropped%29.jpg',
+    'https://imageio.forbes.com/specials-images/imageserve/6059ea4e5a0beaf622a42a20/Megan-Rapinoe---United-States-v-Ireland---International-Friendly/960x0.jpg?height=549&width=711&fit=bounds',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc4dmLL4tmUO9WJsCg7DWXPMBTakr27xtxIQ&s',
   ];
+
+  // Calcular número de columnas según el ancho de la pantalla
+  const screenWidth = Dimensions.get('window').width;
+  const imageWidth = 100;  // Ancho de cada imagen
+  const margin = 10; // Margen entre imágenes
+  const numColumns = Math.floor(screenWidth / (imageWidth + margin * 2));  // Ajustamos el número de columnas
 
   const renderItem = ({ item }) => (
     <Pressable onPress={() => handleImageSelect(item)} style={styles.imageContainer}>
@@ -73,12 +85,12 @@ const ProfilePictureScreen = ({ route }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Selecciona tu foto de perfil</Text>
-      
+
       <FlatList
         data={profileImages}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
-        horizontal
+        numColumns={numColumns}  // Usamos el cálculo dinámico para las columnas
         contentContainerStyle={styles.imageList}
       />
 
@@ -103,10 +115,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   imageList: {
-    marginBottom: 30,
+    width: '100%',  // Asegura que el contenedor ocupe todo el ancho disponible
+    justifyContent: 'flex-start',  // Alinea las imágenes a la izquierda
+    marginLeft: -10,  // Ajusta el margen izquierdo para desplazar todo a la izquierda
   },
   imageContainer: {
-    marginHorizontal: 10,
+    margin: 10,  // Añadido margen entre las imágenes
     alignItems: 'center',
   },
   image: {
@@ -135,3 +149,7 @@ const styles = StyleSheet.create({
 });
 
 export default ProfilePictureScreen;
+
+
+
+
